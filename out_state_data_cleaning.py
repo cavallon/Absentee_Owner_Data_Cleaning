8002c96312b9eed10010921789bfcfacab3918c5
cavallon <cjavallon@hotmail.com>
2024-08-19 18:30:44 -0400

    updated code

--- out_state_data_cleaning.py
+++ out_state_data_cleaning.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import scipy.stats as st
 
 #import out of state owners file (must update the path for each file you need to process):
-file_path = Path("C:/Users/cjava/OneDrive/Desktop/Challenges/Absentee_Owner_Data_Cleaning/Resources/burlington_county_absentee_owners.csv")
+file_path = Path("C:/Users/chris/Desktop/Data_Projects/Absentee_Owner_Data_Cleaning/Resources/burlington_county_absentee_owners.csv")
 
 #create dataframe:
 state_data = pd.read_csv(file_path)
@@ -23,17 +23,23 @@ state_data.rename(columns = {'Property Address Formatted': 'Property Address', '
 state_data.drop(["Tax ID", "Current Owner Name", "Owner Do Not Mail YN"], axis=1, inplace=True)
 
 #drop .0 from owner zip code column:
-state_data['Owner Zip Code'] = state_data['Owner Zip Code'].astype(str).replace('\.0', '', regex=True)
+state_data['Owner Zip Code'] = state_data['Owner Zip Code'].astype(str).replace('/.0', '', regex=True)
 
 #correct all zip codes by adding the leading 0:
 state_data['Owner Zip Code'] = state_data['Owner Zip Code'].apply(lambda x: '{0:0>5}'.format(x))
 state_data['Property Zip Code'] = state_data['Property Zip Code'].apply(lambda x: '{0:0>5}'.format(x))
 
+#reorder columns to a more logical order:
+state_data = state_data[["Property Address", "Property City", "Property State", "Property Zip Code", "Property Zip 4", 
+             "Owner Label Name", "Owner Address", "Owner City State", "Owner Zip Code", "Owner Zip 4"]] 
+
 #sort data by property city:
 state_data.sort_values(by=['Property City'], inplace=True)
 
 #select city for final mailing list(If needed. Otherwise comment this line out for the full list):
-city_data = state_data.loc[state_data['Property City'] == "Willingboro"]
+city_data = state_data.loc[state_data['Property City'] == "Burlington"]
 
 #export finalized data to csv for mailing (update title depending on final list, and change city_data to state_data if full list is needed):
-city_data.to_csv('willingboro_absentee_owner_mailing_list.csv')
+city_data.to_csv('Burlington_absentee_owner_mailing_list.csv')
+
+print('File cleaned & created successfully.')
